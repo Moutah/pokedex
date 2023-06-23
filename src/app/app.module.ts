b5cbeef8a2ccba843ced1d0a9cc89f5e8c46b121
Professor Oak <professor.oak@ikmail.com>
2023-06-23 16:26:37 +0200

    Added API_URL injection token

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,9 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+import { environment } from '@environment';
+import { API_URL } from '@shared/data';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LayoutModule } from './core/layout/layout.module';
@@ -16,7 +19,7 @@ import { LayoutModule } from './core/layout/layout.module';
     HttpClientModule,
     LayoutModule,
   ],
-  providers: [],
+  providers: [{ provide: API_URL, useValue: environment.apiUrl }],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
