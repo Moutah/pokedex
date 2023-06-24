8a02da35b387e3cbeb5728d50654ca46f4913b73
Professor Oak <professor.oak@ikmail.com>
2023-06-24 14:59:49 +0200

    Login page plugged to state

--- src/app/modules/login/components/login-page/login-page.component.ts
+++ src/app/modules/login/components/login-page/login-page.component.ts
@@ -1,12 +1,14 @@
 import { NgIf } from '@angular/common';
-import { Component, inject } from '@angular/core';
+import { Component, inject, OnDestroy } from '@angular/core';
 import { FormControl, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
 import { Router } from '@angular/router';
-import { AuthService } from '@shared/services';
+import { TrainerLogin } from '@core/state/trainer';
+import { TrainerState } from '@core/state/trainer';
+import { Store } from '@ngxs/store';
 
 @Component({
   standalone: true,
@@ -23,18 +25,39 @@ import { AuthService } from '@shared/services';
   templateUrl: './login-page.component.html',
   styleUrls: ['./login-page.component.scss'],
 })
-export class LoginPageComponent {
+export class LoginPageComponent implements OnDestroy {
+  store = inject(Store);
+  router = inject(Router);
+
   tokenControl = new FormControl('', Validators.required);
   isDisplayToken = false;
-  authService = inject(AuthService);
-  router = inject(Router);
+  isInvalidToken = false;
+
+  private hasSubmitted = false;
+
+  private readonly trainerStatusSubscription = this.store
+    .select(TrainerState.status)
+    .subscribe((status) => {
+      if (status === 'connected') {
+        void this.router.navigate(['pokedex', 'list']);
+        return;
+      }
+
+      if (status === 'disconnected' && this.hasSubmitted) {
+        this.isInvalidToken = true;
+      }
+    });
+
+  ngOnDestroy() {
+    this.trainerStatusSubscription.unsubscribe();
+  }
 
   onSubmit() {
     if (!this.tokenControl.value) {
       return;
     }
 
-    this.authService.login(this.tokenControl.value);
-    void this.router.navigate(['/']);
+    this.hasSubmitted = true;
+    this.store.dispatch(new TrainerLogin(this.tokenControl.value));
   }
 }
