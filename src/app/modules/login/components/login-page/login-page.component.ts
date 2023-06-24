60e05f3f7df1f375b6e8ebc66b66adfc44cec9af
Professor Oak <professor.oak@ikmail.com>
2023-06-24 13:17:29 +0200

    Setup login page

--- src/app/modules/login/components/login-page/login-page.component.ts
+++ src/app/modules/login/components/login-page/login-page.component.ts
@@ -1,10 +1,40 @@
-import { Component } from '@angular/core';
+import { NgIf } from '@angular/common';
+import { Component, inject } from '@angular/core';
+import { FormControl, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { Router } from '@angular/router';
+import { AuthService } from '@shared/services';
 
 @Component({
   standalone: true,
-  imports: [],
+  imports: [
+    FormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    ReactiveFormsModule,
+    NgIf,
+    MatButtonModule,
+    MatIconModule,
+  ],
   selector: 'app-login-page',
   templateUrl: './login-page.component.html',
   styleUrls: ['./login-page.component.scss'],
 })
-export class LoginPageComponent {}
+export class LoginPageComponent {
+  tokenControl = new FormControl('', Validators.required);
+  isDisplayToken = false;
+  authService = inject(AuthService);
+  router = inject(Router);
+
+  onSubmit() {
+    if (!this.tokenControl.value) {
+      return;
+    }
+
+    this.authService.login(this.tokenControl.value);
+    void this.router.navigate(['/']);
+  }
+}
