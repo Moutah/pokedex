0ee60ff14d877e32f76ee37cf4fe559edc1bf9f8
Professor Oak <professor.oak@ikmail.com>
2023-06-28 00:06:18 +0200

    Added species searchbar

--- src/app/modules/pokedex/components/species-list-page/species-list-page.component.ts
+++ src/app/modules/pokedex/components/species-list-page/species-list-page.component.ts
@@ -1,14 +1,17 @@
 import { CommonModule } from '@angular/common';
-import { Component, inject, OnInit } from '@angular/core';
+import { Component, inject, OnDestroy, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { MatDialog, MatDialogModule } from '@angular/material/dialog';
 import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { RouterLink } from '@angular/router';
 import { DiscoverSpeciesModalComponent } from '@modules/pokedex/components/discover-modal/discover-species-modal.component';
 import { GetSpeciesList } from '@modules/pokedex/state/species/species.actions';
 import { SpeciesState } from '@modules/pokedex/state/species/species.state';
 import { Store } from '@ngxs/store';
+import { debounceTime, distinctUntilChanged, filter, map, Subject } from 'rxjs';
 
 @Component({
   standalone: true,
@@ -19,23 +22,47 @@ import { Store } from '@ngxs/store';
     MatIconModule,
     MatDialogModule,
     RouterLink,
+    MatInputModule,
+    FormsModule,
   ],
   selector: 'app-species-list-page',
   templateUrl: 'species-list-page.component.html',
   styleUrls: ['species-list-page.component.scss'],
 })
-export class SpeciesListPageComponent implements OnInit {
+export class SpeciesListPageComponent implements OnInit, OnDestroy {
   store = inject(Store);
   dialog = inject(MatDialog);
 
   speciesList$ = this.store.select(SpeciesState.speciesList);
   speciesStatus$ = this.store.select(SpeciesState.status);
 
+  searchChanged$ = new Subject<void>();
+  search = '';
+  searchSubscription = this.searchChanged$
+    .pipe(
+      debounceTime(400),
+      map(() => this.search),
+      filter((search) => search?.length >= 3),
+      distinctUntilChanged()
+    )
+    .subscribe((search) => {
+      this.store.dispatch(new GetSpeciesList(search));
+    });
+
   ngOnInit() {
     this.store.dispatch(new GetSpeciesList());
   }
 
+  ngOnDestroy() {
+    this.searchSubscription.unsubscribe();
+  }
+
   openDiscoverModal() {
     this.dialog.open(DiscoverSpeciesModalComponent);
+  }
+
+  clearSearch() {
+    this.search = '';
+    this.store.dispatch(new GetSpeciesList());
   }
 }
