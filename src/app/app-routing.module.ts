251eff3facf1215b2d69a823461cca4fa5fff928
Professor Oak <professor.oak@ikmail.com>
2023-06-28 20:03:41 +0200

    Replace default route to load dummy component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,43 +1,23 @@
 import { NgModule } from '@angular/core';
 import { provideRouter, RouterModule, Routes, withComponentInputBinding } from '@angular/router';
+import { DummyComponent } from '@core/components/dummy/dummy.component';
 import { LoginPageComponent } from '@modules/login/components/login-page/login-page.component';
 import { LoginModule } from '@modules/login/login.module';
-import { SpeciesListPageComponent } from '@modules/pokedex/components/species-list-page/species-list-page.component';
-import { SpeciesPageComponent } from '@modules/pokedex/components/species-page/species-page.component';
-import { PokedexModule } from '@modules/pokedex/pokedex.module';
 
 const routes: Routes = [
   {
     path: '',
     pathMatch: 'full',
-    redirectTo: 'species',
+    component: DummyComponent,
   },
   {
     path: 'login',
     component: LoginPageComponent,
   },
-  {
-    path: 'species',
-    children: [
-      {
-        path: '',
-        pathMatch: 'full',
-        redirectTo: 'list',
-      },
-      {
-        path: 'list',
-        component: SpeciesListPageComponent,
-      },
-      {
-        path: ':speciesId',
-        component: SpeciesPageComponent,
-      },
-    ],
-  },
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes), LoginModule, PokedexModule],
+  imports: [RouterModule.forRoot(routes), LoginModule],
   providers: [provideRouter(routes, withComponentInputBinding())],
 })
 export class AppRoutingModule {}
