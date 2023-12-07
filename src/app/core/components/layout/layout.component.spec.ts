832653f5c4d41a95cfdabc8a41b2902183534308
Mathieu Tappolet <mathieu.tappolet@infomaniak.com>
2023-12-07 16:55:42 +0100

    Fix Lint/Format

--- src/app/core/components/layout/layout.component.spec.ts
+++ src/app/core/components/layout/layout.component.spec.ts
@@ -1,6 +1,5 @@
 import { HttpClientModule } from '@angular/common/http';
 import { TestBed } from '@angular/core/testing';
-import { MatDialog } from '@angular/material/dialog';
 import { RouterTestingModule } from '@angular/router/testing';
 import { LayoutComponent } from '@core/components/layout/layout.component';
 import { TrainerResetModalComponent } from '@core/components/trainer-reset-modal/trainer-reset-modal.component';
